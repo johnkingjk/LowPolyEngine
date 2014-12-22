33573b253e22e4c8191dd0ca82c9af73e6069108
michidk <michidk@live.de>
2014-12-22 18:14:40 +0100

    added Camera.java

---
+++ src/main/java/engine/Camera.java
@@ -0,0 +1,50 @@
+package engine;
+
+import org.lwjgl.util.vector.Vector3f;
+
+/**
+ * Copyright by michidk
+ * Created: 22.12.2014.
+ */
+public class Camera {
+
+    private Vector3f position;
+    private Vector3f pitch;
+    private Vector3f yaw;
+
+    public Camera(Vector3f position) {
+        this.position = position;
+        this.pitch = new Vector3f(0, 0, 0);
+        this.yaw = new Vector3f(0, 0, 0);
+    }
+
+    public Camera(Vector3f position, Vector3f pitch, Vector3f yaw) {
+        this.position = position;
+        this.pitch = pitch;
+        this.yaw = yaw;
+    }
+
+    public Vector3f getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector3f position) {
+        this.position = position;
+    }
+
+    public Vector3f getPitch() {
+        return pitch;
+    }
+
+    public void setPitch(Vector3f pitch) {
+        this.pitch = pitch;
+    }
+
+    public Vector3f getYaw() {
+        return yaw;
+    }
+
+    public void setYaw(Vector3f yaw) {
+        this.yaw = yaw;
+    }
+}
