6b7083d7b72a44680195c2a3013881dbc81d475f
michidk <michidk@live.de>
2014-12-25 16:44:39 +0100

    added inputmanager

--- src/main/java/engine/LowPolyEngine.java
+++ src/main/java/engine/LowPolyEngine.java
@@ -1,5 +1,7 @@
 package engine;
 
+import engine.input.InputManager;
+import engine.input.MouseButton;
 import engine.rendering.OpenGLLoader;
 import engine.math.Matrix4f;
 import engine.math.Vector3f;
@@ -9,6 +11,7 @@ import engine.rendering.model.ModelLoader;
 import engine.rendering.shader.DefaultShader;
 import engine.transform.Camera;
 import engine.transform.Transform;
+import net.java.games.input.Mouse;
 import org.lwjgl.opengl.*;
 
 /**
@@ -26,6 +29,8 @@ public class LowPolyEngine {
                         60
                         )
         );
+
+        InputManager.init();
 
         OpenGLLoader loader = new OpenGLLoader();
         DefaultShader shader = new DefaultShader();
@@ -55,6 +60,7 @@ public class LowPolyEngine {
         //GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_LINE);
 
         while(!Display.isCloseRequested()) {
+            
             GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 
             camera.update();
@@ -96,10 +102,12 @@ public class LowPolyEngine {
             shader.stop();
 
             DisplayManager.update();
+            InputManager.update();
         }
 
         loader.cleanUp();
         shader.cleanUp();
         DisplayManager.destroy();
+        InputManager.destroy();
     }
 }
