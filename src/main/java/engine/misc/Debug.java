c7398dd483f93f20bfd0dc759f310d0c68aee288
michidk <michidk@live.de>
2014-12-26 17:25:01 +0100

    added Debug helper/logger

---
+++ src/main/java/engine/misc/Debug.java
@@ -0,0 +1,56 @@
+package engine.misc;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Copyright by michidk
+ * Created: 26.12.2014.
+ */
+public class Debug {
+
+    private static final Logger logger = Logger.getLogger("LowPolyEngine");
+
+    private static final String SPACER = " - ";
+
+    public static void LogInfo(String message) {
+        logger.log(Level.INFO, message);
+    }
+
+    public static void LogWarning(String message) {
+        logger.log(Level.WARNING, message);
+    }
+
+    public static void LogError(String message) {
+        logger.log(Level.SEVERE, message);
+    }
+
+    public static void Log(Level level, String message) {
+        logger.log(level, message);
+    }
+
+    public static void Log(Level level, String... messages) {
+        StringBuilder sb = new StringBuilder();
+        for (String s : messages) {
+            sb.append(s);
+            sb.append(SPACER);
+        }
+        String complete = sb.toString();
+        logger.log(level, complete.substring(complete.length() - SPACER.length()));
+    }
+
+    public static void Log(Level level, Object object) {
+        logger.log(level, object.toString());
+    }
+
+    public static void Log(Level level, Object... objects) {
+        StringBuilder sb = new StringBuilder();
+        for (Object o : objects) {
+            sb.append(o.toString());
+            sb.append(SPACER);
+        }
+        String complete = sb.toString();
+        logger.log(level, complete.substring(complete.length() - SPACER.length()));
+    }
+
+}
